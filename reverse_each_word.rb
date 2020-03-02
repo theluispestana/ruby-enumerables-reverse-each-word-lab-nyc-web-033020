b9aa362d8e0c74ac6493b156de3572910eb0b4eb
Luis Pestana <lpestana@protonmail.com>
2020-03-02 16:34:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   string_arr = string.split(' ')
-  string_arr.each{ |word| word.reverse }
+  string string_arr.each{ |word| word.reverse }
   string_arr.join(' ')
 end
